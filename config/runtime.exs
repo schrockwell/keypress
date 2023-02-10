312f729862ced77abc6f2937ecf1480e1406be96
Rockwell Schrock <rockwell@schrock.me>
2023-02-09 23:08:49 -0500

    Add Dokku config

--- config/runtime.exs
+++ config/runtime.exs
@@ -34,7 +34,9 @@ if config_env() == :prod do
     # ssl: true,
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    socket_options: maybe_ipv6
+    socket_options: maybe_ipv6,
+    ssl: true,
+    maintenance_database: "defaultdb"
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
@@ -48,7 +50,7 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
+  host = System.get_env("WEB_HOST") || "example.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :keypress, KeypressWeb.Endpoint,
