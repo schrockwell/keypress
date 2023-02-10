382e5caf430d12350d5f9e9c560c5e3ab177786a
Rockwell Schrock <rockwell@schrock.me>
2023-02-09 22:25:58 -0500

    Improve Theme delegation to be more explicit and have component attrs

--- lib/keypress_web/theme.ex
+++ lib/keypress_web/theme.ex
@@ -1,15 +1,40 @@
 defmodule KeypressWeb.Theme do
-  @themes %{"tailwind" => KeypressWeb.TailwindTheme}
-  @components [:app, :post_preview]
+  use KeypressWeb, :html
 
-  defp theme_component(component, %{theme: theme} = assigns) do
-    theme_module = Map.fetch!(@themes, theme)
-    apply(theme_module, component, [assigns])
+  alias Keypress.Schemas.Post
+
+  @themes %{"tailwind" => KeypressWeb.TailwindTheme}
+
+  @callback body_attrs :: %{atom => any}
+  @callback app(assigns :: map) :: Phoenix.LiveView.Rendered.t()
+  @callback post_preview(assigns :: map) :: Phoenix.LiveView.Rendered.t()
+  @callback post_body(assigns :: map) :: Phoenix.LiveView.Rendered.t()
+
+  attr :flash, :map, required: true
+  attr :theme, :string, required: true
+  slot :inner_block, required: true
+
+  def app(%{theme: theme} = assigns) do
+    @themes[theme].app(assigns)
   end
 
-  for component <- @components do
-    def unquote(component)(assigns) do
-      theme_component(unquote(component), assigns)
-    end
+  attr :theme, :string, required: true
+  attr :post, Post, required: true
+  attr :class, :any, default: nil
+  attr :editable, :boolean, default: false
+
+  def post_preview(%{theme: theme} = assigns) do
+    @themes[theme].post_preview(assigns)
+  end
+
+  attr :theme, :string, required: true
+  attr :post, Post, required: true
+
+  def post_body(%{theme: theme} = assigns) do
+    @themes[theme].post_body(assigns)
+  end
+
+  def body_attrs(theme) do
+    @themes[theme].body_attrs()
   end
 end
--- lib/keypress_web/themes/tailwind_theme.ex
+++ lib/keypress_web/themes/tailwind_theme.ex
@@ -1,18 +1,10 @@
 defmodule KeypressWeb.TailwindTheme do
   use KeypressWeb, :html
-
-  alias Keypress.Schemas.Post
+  @behaviour KeypressWeb.Theme
 
   embed_templates "tailwind/*"
 
-  attr :class, :any, default: nil
-  def post_preview(assigns)
-
-  attr :post, Post, required: true
-
-  def post_body(assigns) do
-    ~H"""
-    <%= raw(Earmark.as_html!(@post.body, %Earmark.Options{code_class_prefix: "language-"})) %>
-    """
+  def body_attrs do
+    %{class: "bg-white antialiased"}
   end
 end
